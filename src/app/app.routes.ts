594e5ae722380afd8d59f04ef9c94620a22a80fe
Villy Rosa <villyrosa69@gmail.com>
2025-12-12 13:45:03 -0300

    feat: create home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import authRoutes from './features/auth/auth.routes';
+import {Home} from '@features/home/home';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
   ...authRoutes
 ];
