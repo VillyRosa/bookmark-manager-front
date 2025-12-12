1eab250e8e5cc41cf838846988a4191cbecb52af
Villy Rosa <villyrosa69@gmail.com>
2025-12-12 00:16:35 -0300

    feat: create interface for login page

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { InputComponent } from '@shared/components/input/input';
+import { Button } from '@shared/components/button/button';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    InputComponent,
+    RouterModule,
+    Button,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  private router = inject(Router);
 
+  protected loginForm: FormGroup = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  protected onSubmit(): void {
+    this.router.navigateByUrl('/');
+  }
 }
