b51568bdfa77f799f078442c8b0eef2e1c81a3f2
Villy Rosa <villyrosa69@gmail.com>
2025-12-12 13:32:10 -0300

    feat: create reset-password page

--- src/app/features/auth/auth.routes.ts
+++ src/app/features/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Register } from './register/register';
 import {ForgotPassword} from '@features/auth/forgot-password/forgot-password';
+import {ResetPassword} from '@features/auth/reset-password/reset-password';
 
 const routes: Routes = [
   {
@@ -10,6 +11,7 @@ const routes: Routes = [
       { path: 'login', component: Login },
       { path: 'register', component: Register },
       { path: 'forgot-password', component: ForgotPassword },
+      { path: 'reset-password', component: ResetPassword }
     ]
   }
 ];
---
+++ src/app/features/auth/reset-password/reset-password.ts
@@ -0,0 +1,30 @@
+import {Component, inject} from '@angular/core';
+import {Button} from "@shared/components/button/button";
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {InputComponent} from "@shared/components/input/input";
+import {Router, RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-reset-password',
+  imports: [
+    Button,
+    FormsModule,
+    InputComponent,
+    RouterLink,
+    ReactiveFormsModule
+  ],
+  templateUrl: './reset-password.html',
+  styleUrl: './reset-password.scss',
+})
+export class ResetPassword {
+  private router = inject(Router);
+
+  protected resetPasswordForm: FormGroup = new FormGroup({
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required])
+  });
+
+  protected onSubmit(): void {
+    this.router.navigateByUrl('/auth/login');
+  }
+}
