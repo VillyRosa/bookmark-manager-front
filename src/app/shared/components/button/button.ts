fde8b91b5a6d6cd0de8619b889e055ac88ad5577
Villy Rosa <villyrosa@hotmail.com>
2025-12-13 20:01:54 -0300

    feat: create secondary, error and sm states for button components

--- src/app/shared/components/button/button.ts
+++ src/app/shared/components/button/button.ts
@@ -1,11 +1,17 @@
 import { Component, Input } from '@angular/core';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-button',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './button.html',
   styleUrl: './button.scss',
 })
 export class Button {
   @Input() type: 'button' | 'submit' = 'button';
+  @Input() hierarchy: 'primary' | 'secondary' | 'error' = 'primary';
+  @Input() size: 'md' | 'sm' = 'md';
+  @Input() onlyIcon: boolean = false;
 }
