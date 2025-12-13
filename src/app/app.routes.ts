fe431212d99c01f20311af282e9a8ff1a38277de
Villy Rosa <villyrosa69@gmail.com>
2025-12-13 10:16:22 -0300

    feat: create main-layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import authRoutes from './features/auth/auth.routes';
 import {Home} from '@features/home/home';
+import {Archived} from '@features/archived/archived';
+import {MainLayout} from './core/layouts/main-layout/main-layout';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: Home },
+      { path: 'archived', component: Archived },
+    ]
+  },
   ...authRoutes
 ];
